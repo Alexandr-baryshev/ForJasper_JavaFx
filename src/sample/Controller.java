7be5f798eaea815064463bb64f6078898a8cafb3
Alexandr <alexandr.baryshev@hotmail.com>
2020-04-16 15:43:56 +1000

    sava

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,96 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField rStart_p;
+
+    @FXML
+    private TextField rSize_p;
+
+    @FXML
+    private TextField cStart_p;
+
+    @FXML
+    private TextField cSize_p;
+
+    @FXML
+    private TextField sort_RC;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private TextArea inputTexField;
+
+    @FXML
+    private TextArea outText;
+
+    @FXML
+    void initialize() {
+        startButton.setOnAction(event -> {
+            Integer rStart = new Integer(rStart_p.getText());
+            Integer rSize = new Integer(rSize_p.getText());
+            Integer cStart = new Integer(cStart_p.getText());
+            Integer cSize = new Integer(cSize_p.getText());
+            String inputText = inputTexField.getText();
+
+
+        outText.setText(sort_C(inputText,rStart,rSize,cStart,cSize));
+
+        });
+    }
+
+
+
+
+    static String vTest = "\t<variable name=\"R1 C1\" class=\"java.lang.Integer\" calculation=\"DistinctCount\">\n" +
+            "\t\t<variableExpression><![CDATA[$V{R1} && $V{C1} ? $F{Идентификатор происшествия} : null]]></variableExpression>\n" +
+            "\t</variable>";
+
+    static String sort_C(String variable,int rStart, int rSize, int cStart, int cSize){
+        String row = "", column = "", allResult = "";
+
+        int c = cStart;
+        while (c <= cSize) {
+            column = variable.replace("C1", "C" + c);
+
+            int r = rStart;
+            while (r <= rSize){
+                row = column.replace("R1", "R" + r);
+                allResult = allResult + row + "\n";
+                r++;
+            }
+            c++;
+        }
+        return(allResult);
+    }
+
+
+    static String sort_R(String variable, int rStart, int rSize, int cStart, int cSize){
+        String row = "", column = "", allResult = "";
+
+        for (int r = rStart; r <= rSize; r+=1) {
+            row = variable.replace("R1", "R" + r);
+
+            for (int c = cStart; c <= cSize; c+=1){
+                column = row.replace("C1", "C" + c);
+                allResult = allResult + column + "\n";
+            }
+        }
+        return(allResult);
+    }
 }

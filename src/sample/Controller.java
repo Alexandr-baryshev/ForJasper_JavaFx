4fd603060f1f0a14d3cb080e0b2cc7324b4a879d
Alexandr <alexandr.baryshev@hotmail.com>
2020-04-16 16:40:55 +1000

    save

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,83 +10,88 @@ import java.util.ResourceBundle;
 
 public class Controller {
 
-    @FXML
-    private ResourceBundle resources;
+   @FXML
+   private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+   @FXML
+   private URL location;
 
-    @FXML
-    private TextField rStart_p;
+   @FXML
+   private TextField rStart_p;
 
-    @FXML
-    private TextField rSize_p;
+   @FXML
+   private TextField rSize_p;
 
-    @FXML
-    private TextField cStart_p;
+   @FXML
+   private TextField cStart_p;
 
-    @FXML
-    private TextField cSize_p;
+   @FXML
+   private TextField cSize_p;
 
-    @FXML
-    private TextField sort_RC;
+   @FXML
+   private TextField sort_RC;
 
-    @FXML
-    private Button startButton;
+   @FXML
+   private Button startButton;
 
-    @FXML
-    private TextArea inputTexField;
+   @FXML
+   private TextArea inputTexField;
 
-    @FXML
-    private TextArea outText;
+   @FXML
+   private TextArea outText;
 
-    @FXML
-    void initialize() {
-        startButton.setOnAction(event -> {
-            Integer rStart = new Integer(rStart_p.getText());
-            Integer rSize = new Integer(rSize_p.getText());
-            Integer cStart = new Integer(cStart_p.getText());
-            Integer cSize = new Integer(cSize_p.getText());
-            String inputText = inputTexField.getText();
+   @FXML
+   void initialize() {
+      startButton.setOnAction(event -> {
+         int rStart = new Integer(rStart_p.getText());
+         int rSize = new Integer(rSize_p.getText());
+         int cStart = new Integer(cStart_p.getText());
+         int cSize = new Integer(cSize_p.getText());
+         String inputText = inputTexField.getText();
 
-
+         if (sort_RC.getText().equals("R")) {
+            outText.setText(sort_R(inputText, rStart, rSize, cStart, cSize));
+         } else if (sort_RC.getText().equals("C")) {
             outText.setText(sort_C(inputText, rStart, rSize, cStart, cSize));
+         } else {
+            outText.setText("Введите приоритет сортировки R или C");
+         }
 
 
-        });
-    }
+      });
+   }
 
 
-    static String sort_C(String variable, int rStart, int rSize, int cStart, int cSize) {
-        String row = "", column = "", allResult = "";
+   static String sort_C(String variable, int rStart, int rSize, int cStart, int cSize) {
+      String row = "", column = "", allResult = "";
 
-        int c = cStart;
-        while (c <= cSize) {
-            column = variable.replace("C1", "C" + c);
+      int c = cStart;
+      while (c <= cSize) {
+         column = variable.replace("C1", "C" + c);
 
-            int r = rStart;
-            while (r <= rSize) {
-                row = column.replace("R1", "R" + r);
-                allResult = allResult + row + "\n";
-                r++;
-            }
-            c++;
-        }
-        return (allResult);
-    }
+         int r = rStart;
+         while (r <= rSize) {
+            row = column.replace("R1", "R" + r);
+            allResult = allResult + row + "\n";
+            r++;
+         }
+         c++;
+      }
+      return (allResult);
+   }
 
 
-    static String sort_R(String variable, int rStart, int rSize, int cStart, int cSize) {
-        String row = "", column = "", allResult = "";
+   static String sort_R(String variable, int rStart, int rSize, int cStart, int cSize) {
+      String row = "", column = "", allResult = "";
 
-        for (int r = rStart; r <= rSize; r += 1) {
-            row = variable.replace("R1", "R" + r);
+      for (int r = rStart; r <= rSize; r += 1) {
+         row = variable.replace("R1", "R" + r);
 
-            for (int c = cStart; c <= cSize; c += 1) {
-                column = row.replace("C1", "C" + c);
-                allResult = allResult + column + "\n";
-            }
-        }
-        return (allResult);
-    }
+         for (int c = cStart; c <= cSize; c += 1) {
+            column = row.replace("C1", "C" + c);
+            allResult = allResult + column + "\n";
+         }
+      }
+      return (allResult);
+   }
 }
